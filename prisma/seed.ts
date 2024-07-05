e20e3e85c035ff7f06e850ed1170db8781abe221
Leroyalle <nikolajmelonov@yandex.ru>
2024-07-05 19:10:29 +0300

    сиды юзеров, категорий, ингредиентов и продуктов без ингредиентов

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,27 +1,42 @@
 import { hashSync } from 'bcrypt';
 import { prisma } from './prisma-client';
+import { categories, _ingredients, products } from './constans';
 
 async function up() {
   await prisma.user.createMany({
     data: [
       {
-        fullName: 'User',
+        fullName: 'User Test',
         email: 'user@test.ru',
         password: hashSync('111111', 10),
         verified: new Date(),
         role: 'USER',
       },
       {
-        fullName: 'Admin',
-        email: 'user@test.ru',
+        fullName: 'Admin Test',
+        email: 'admin@test.ru',
         password: hashSync('111111', 10),
         verified: new Date(),
-        role: 'USER',
+        role: 'ADMIN',
       },
     ],
   });
+  await prisma.category.createMany({
+    data: categories,
+  });
+  await prisma.ingredient.createMany({
+    data: _ingredients,
+  });
+  await prisma.product.createMany({
+    data: products,
+  });
 }
-async function down() {}
+async function down() {
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Ingredient" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`;
+}
 async function main() {
   try {
     await down();
@@ -30,3 +45,12 @@ async function main() {
     console.error(e);
   }
 }
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
