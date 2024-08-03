4f0c0b6b05f616e84495a57a4da1fa396a26560e
Leroyalle <nikolajmelonov@yandex.ru>
2024-08-03 23:29:27 +0300

    рендер продуктов

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
-import {
-  Container,
-  Title,
-  Categories,
-  SortPopup,
-  SortBar,
-  Filters,
-  ProductCart,
-  ProductsGroupList,
-} from '@/components/shared';
-import { Button } from '@/components/ui';
-import Image from 'next/image';
+import { Container, Title, SortBar, Filters, ProductsGroupList } from '@/components/shared';
+import { prisma } from '@/prisma/prisma-client';
+import { categories, products } from '../prisma/constants';
 
-export default function Home() {
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          items: true,
+          ingredients: true,
+        },
+      },
+    },
+  });
+
   return (
     <>
       <Container className="mt-10">
@@ -26,130 +28,17 @@ export default function Home() {
 
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              <ProductsGroupList
-                title="Пиццы"
-                items={[
-                  {
-                    id: 1,
-                    name: 'Чили',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
-                    price: 123,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 2,
-                    name: 'Чили',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
-                    price: 123,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 3,
-                    name: 'Чили',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
-                    price: 123,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 4,
-                    name: 'Чили',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
-                    price: 123,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 5,
-                    name: 'Чили',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
-                    price: 123,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 6,
-                    name: 'Чили',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
-                    price: 123,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 7,
-                    name: 'Чили',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
-                    price: 123,
-                    items: [{ price: 550 }],
-                  },
-                ]}
-                categoryId={1}
-              />
-              <ProductsGroupList
-                title="Комбо"
-                items={[
-                  {
-                    id: 1,
-                    name: 'Чили',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
-                    price: 123,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 2,
-                    name: 'Чили',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
-                    price: 123,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 3,
-                    name: 'Чили',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
-                    price: 123,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 4,
-                    name: 'Чили',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
-                    price: 123,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 5,
-                    name: 'Чили',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
-                    price: 123,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 6,
-                    name: 'Чили',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
-                    price: 123,
-                    items: [{ price: 550 }],
-                  },
-                  {
-                    id: 7,
-                    name: 'Чили',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:292x292/11EE7D6149EB101D8727573088FA2EFF.avif',
-                    price: 123,
-                    items: [{ price: 550 }],
-                  },
-                ]}
-                categoryId={2}
-              />
+              {categories.map(
+                (category) =>
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      items={category.products}
+                      categoryId={category.id}
+                    />
+                  ),
+              )}
             </div>
           </div>
         </div>
