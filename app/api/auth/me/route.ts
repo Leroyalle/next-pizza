953d0938ca557f41574bee182f6590194a9c4f68
Leroyalle <nikolajmelonov@yandex.ru>
2024-08-29 21:36:33 +0300

    fix

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -5,9 +5,9 @@ import { NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic';
 
-export async function GET(req: any, res: any) {
+export async function GET(req: Request) {
   try {
-    const user = await getServerSession(req, res, authOptions);
+    const user = await getServerSession({ req, ...authOptions });
 
     if (!user) {
       return NextResponse.json({ message: 'Вы не авторизованы' }, { status: 401 });
