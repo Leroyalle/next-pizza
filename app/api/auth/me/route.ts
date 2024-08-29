84bccbd508d2b1cad79efac81ce22815b3a3b674
Leroyalle <nikolajmelonov@yandex.ru>
2024-08-29 20:31:47 +0300

    fix

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,10 +1,14 @@
 import { prisma } from '@/prisma/prisma-client';
+import { authOptions } from '@/shared/constants/auth-options';
 import { getUserSession } from '@/shared/lib/get-user-session';
+import { getServerSession } from 'next-auth/next';
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export const dynamic = 'force-dynamic';
+
+export async function GET(req: any, res: any) {
   try {
-    const user = await getUserSession();
+    const user = await getServerSession(req, res, authOptions);
 
     if (!user) {
       return NextResponse.json({ message: 'Вы не авторизованы' }, { status: 401 });
