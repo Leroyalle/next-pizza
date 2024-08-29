0db38db01cbed88190aa2c8ddd71e577b71dd5af
Leroyalle <nikolajmelonov@yandex.ru>
2024-08-29 04:26:46 +0300

    автовставка личных данных в оформление заказа

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -15,10 +15,12 @@ import { checkoutFormSchema, CheckoutFormValues } from '@/shared/constants';
 import { createOrder } from '@/app/actions';
 import toast from 'react-hot-toast';
 import { useSession } from 'next-auth/react';
+import { Api } from '@/shared/services/api-client';
 
 export default function CheckoutPage() {
   const { totalAmount, items, updateCartItem, removeCartItem, loading } = useCart();
   const [submitting, setSubmitting] = React.useState(false);
+  const { data: session } = useSession();
 
   const form = useForm<CheckoutFormValues>({
     resolver: zodResolver(checkoutFormSchema),
@@ -31,6 +33,19 @@ export default function CheckoutPage() {
       comment: '',
     },
   });
+
+  React.useEffect(() => {
+    async function fetchUserInfo() {
+      const data = await Api.auth.getMe();
+      const [firstName, lastName] = data.fullName.split(' ');
+      form.setValue('firstName', firstName);
+      form.setValue('lastName', lastName);
+      form.setValue('email', data.email);
+    }
+    if (session) {
+      fetchUserInfo();
+    }
+  }, [session]);
 
   const onClickCountButton = (id: number, quantity: number, type: 'plus' | 'minus') => {
     const newQuantity = type === 'plus' ? quantity + 1 : quantity - 1;
