be880a9c57d8ee4426486495622685d60fb3a97b
Leroyalle <nikolajmelonov@yandex.ru>
2024-11-14 01:39:02 +0300

    fix

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import { Header } from '@/shared/components/shared';
 import { Suspense } from 'react';
 
 export const metadata: Metadata = {
-  title: 'More Pro Pizza',
+  title: 'Pizza Slice',
 };
 
 export default function HomeLayout({
